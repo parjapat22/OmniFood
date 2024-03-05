03675e854ee8ec1164e28f98680f3c5b9c8a8c9e
parjapat22 <bsntparjapat8@gmail.com>
2024-03-05 20:48:54 +0000

    javascript added

---
+++ ReadMe2.md
@@ -0,0 +1,31 @@
+# Omni Food Responsive Website
+
+## Tech Stack
+
+HTML5, CSS3, JavaScript
+
+## Key Features
+
+### Responsiveness
+
+Supports responsiveness for Desktop, Laptop, Tablet and Mobile Phone.
+
+### Sticky Main Navigation
+
+Sticky main navigation bar is implemented using JavaScript.
+
+### Hamburger Menu icon
+
+Hamburger menu icon is implemented using JavaScript and fully functional on mobile phones.
+
+### Compatibility
+
+This website is compatible with most popular browsers.
+
+## Project Type
+
+Practice project
+
+## Reference
+
+This project is inspired from a course available on Udemy.
---
+++ js/script.js
@@ -0,0 +1,80 @@
+// add current year to copyright in footer section
+const yearEl = document.querySelector(".year");
+yearEl.textContent = new Date().getFullYear();
+
+// mobile navigation
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+// fixing smooth scroll in Safari
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const href = link.getAttribute("href");
+
+    // scroll back to top
+    if (href === "#") {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    }
+
+    // scroll to other sections
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // close mobile navigation
+    if (link.classList.contains("main-nav-link")) {
+      headerEl.classList.remove("nav-open");
+    }
+  });
+});
+
+// sticky navigation
+const sectionHeroEl = document.querySelector(".section-hero");
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("stickyNav");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("stickyNav");
+    }
+  },
+  {
+    // null means viewport
+    root: null,
+    threshold: 0.8,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
+
+// fixing flexbox gap property missing in some Safari versions
+function checkFlexGap() {
+  var flex = document.createElement("div");
+  flex.style.display = "flex";
+  flex.style.flexDirection = "column";
+  flex.style.rowGap = "1px";
+
+  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
+
+  document.body.appendChild(flex);
+  var isSupported = flex.scrollHeight === 1;
+  flex.parentNode.removeChild(flex);
+  console.log(isSupported);
+
+  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+}
+
+checkFlexGap();
